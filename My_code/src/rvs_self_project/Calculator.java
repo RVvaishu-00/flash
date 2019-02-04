b9aeef6311fd6b7671d13abf1a5e760e5b0cee66
Vaishnavi <vaishanavi.k@rapidvaluesolutions.com>
2019-02-04 18:17:11 +0530

    first commit.

---
+++ My_code/src/rvs_self_project/Calculator.java
@@ -0,0 +1,16 @@
+package rvs_self_project;
+
+public class Calculator {
+
+	public int add(int num1, int num2) {
+		
+		int result = num1+num2;
+		return result;
+	}
+	
+public int sub(int num1, int num2) {
+		
+		int result = num1-num2;
+		return result;
+	}
+}
